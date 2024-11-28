c78ad63b0f8601ed417ed8de69c9184421e7012f
asce610 <fitzgames999@gmail.com>
2024-11-28 10:01:43 +0000

    add changes

--- src/pages/EatDetailPage.tsx
+++ src/pages/EatDetailPage.tsx
@@ -1,17 +1,15 @@
 import { IonContent, IonFooter, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import Footer from '../components/general/Footer';
 import Header from '../components/general/Header';
-import NewEat from '../components/eatlist/NewEat';
 import { useEffect, useState } from 'react';
 import { useLocation } from "react-router-dom";
 import { Restaurant } from '../components/shared-preferences';
+import './EatDetailPage.css';
 
 
 const Home: React.FC = () => {
 
   let location = useLocation();
-  const [data, setData] = useState(location.state);
-  console.log(data);
 
   return (
     <IonPage>
@@ -24,7 +22,21 @@ const Home: React.FC = () => {
       
       <IonContent fullscreen>
         <div className="container">
-          <p></p>
+          <p>#{location.state.id}</p>
+          <p>Restaurant: {location.state.name}</p>
+          <p>Type: {location.state.type}</p>
+          <p>Location: {location.state.location}</p>
+          <p>Date: {location.state.date}</p>
+          <p>Aesthetics: V {location.state.vascoAesthetic} L {location.state.laraAesthetic}</p>
+          <p>Taste: V {location.state.vascoTaste} L {location.state.laraTaste}</p>
+          <p>Vibes: V {location.state.vascoVibes} L {location.state.laraVibes}</p>
+          <p>Price: {location.state.price}€</p>
+          <p>Comment: "{location.state.comment}"</p>
+          <p>Return: {location.state.return ? 'Yes' : 'No'}</p>
+        </div>
+        <div className='button-container'>
+          <button>DELETE</button>
+          <button>EDIT</button>
         </div>
       </IonContent>
       
